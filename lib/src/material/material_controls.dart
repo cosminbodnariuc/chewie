f69036d8ba12b4387021014391731b4756a62a86
Cosmin <cosmin@upcode.dev>
2023-03-28 19:31:59 +0300

    Remove pop()

--- lib/src/material/material_controls.dart
+++ lib/src/material/material_controls.dart
@@ -176,7 +176,6 @@ class _MaterialControlsState extends State<MaterialControls> with SingleTickerPr
       child: IconButton(
         onPressed: () async {
           _hideTimer?.cancel();
-          Navigator.of(context).pop();
 
           if (chewieController.optionsBuilder != null) {
             await chewieController.optionsBuilder!(context, options);
